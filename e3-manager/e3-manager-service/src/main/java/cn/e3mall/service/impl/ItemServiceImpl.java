07d728dba68dcdf48161dd82e6163f7947af7a4c
luowenxin <luowenxin456@foxmail.com>
2017-12-04 21:37:26 +0800

    12.4

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.e3mall.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,12 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
 import cn.e3mall.common.pojo.EasyUiDataGridResult;
+import cn.e3mall.common.pojo.TaotaoResult;
+import cn.e3mall.common.utils.IDUtils;
+import cn.e3mall.mapper.TbItemDescMapper;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
 import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 
@@ -22,10 +27,12 @@ import cn.e3mall.service.ItemService;
      * @date 2017年11月27日
      */
 @Service("itemService")
-//@Transactional
+@Transactional
 public class ItemServiceImpl implements ItemService {
 	@Autowired
 	private TbItemMapper itemMapper;
+	@Autowired
+	private TbItemDescMapper itemDescMapper;
 	@Override
 	public TbItem getItemById(Long id) {
 		TbItem selectByPrimaryKey = itemMapper.selectByPrimaryKey(id);
@@ -43,5 +50,28 @@ public class ItemServiceImpl implements ItemService {
 	    long total = pageInfo.getTotal();//设置总记录数	
 	    result.setTotal(total);
 	    return result;
+	}
+	@Override
+	/**
+	 *  生成商品的id
+	 *  设置商品的状态:1正常,2:下架,3:删除
+	 *  填充tbitem的内容
+	 *  填充tbitemDesc
+	 *  
+	 */
+	public TaotaoResult addItem(TbItem tbitem, String desc) {
+		long itemId = IDUtils.genItemId();
+		tbitem.setStatus((byte) 1);
+		Date date = new Date();
+		tbitem.setCreated(date);
+		tbitem.setUpdated(date);
+		tbitem.setId(itemId);
+		itemMapper.insert(tbitem);//插入tbitem
+		TbItemDesc tbItemDesc = new TbItemDesc();
+		tbItemDesc.setItemDesc(desc);
+		tbItemDesc.setCreated(date);
+		tbItemDesc.setItemId(itemId);
+		itemDescMapper.insert(tbItemDesc);
+		return TaotaoResult.ok();
 	}
 }
