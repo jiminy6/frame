37c05b9ad75d58bd2b4b53de2cd1ec4f86be5dc4
luowenxin <luowenxin456@foxmail.com>
2017-12-06 18:21:59 +0800

    昨天的任务

---
+++ bos_fore/src/main/java/cn/itcast/bos_fore/action/CustomerAction.java
@@ -0,0 +1,123 @@
+package cn.itcast.bos_fore.action;
+
+
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import javax.jws.WebParam.Mode;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.ActionContext;
+
+import cn.itcast.bos_fore.utils.*;
+import cn.itcast.crm.domain.Customer;
+@Controller
+public class CustomerAction extends BaseAction<Customer>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	 
+	    /**
+	     * 说明：发送验证码
+	     * @return
+	     * @author luowenxin
+	     * @time：2017年12月5日 下午8:30:23
+	     */
+	//从模型驱动中获取telephone
+	//获取的checkcode存入session中
+	@Action("customer_sendCheckcode")
+//	@Action("aaa")
+	public String sendCheckCode(){
+		String checkcode = RandomStringUtils.randomNumeric(4);
+		ServletActionContext.getRequest().getSession().setAttribute(model.getTelephone(), checkcode);
+		SmsUtils.sendSmsCheckCode(checkcode, model.getTelephone());
+		return NONE;
+	}
+	//属性驱动获得checkcode
+	private String checkcode;
+
+	public void setCheckcode(String checkcode) {
+		this.checkcode = checkcode;
+	}
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
+	 
+	    /**
+	     * 说明：customer注册。首先判断客户输入的验证码和session中的是否一致
+	     * @param customer
+	     * @return
+	     * @author luowenxin
+	     * @time：2017年12月6日 上午12:32:33 value="customer_regist"
+	     */
+	@Action(value="customer_regist",results={
+			@Result(type="redirect",location="/signup-success.html"),
+			@Result( name=INPUT, type="redirect",location="/signup.html")
+			})
+	public String regist(){
+//		String sessionCode = (String) ActionContext.getContext().getSession().get(model.getTelephone());//session中的验证码
+		String sessionCode = (String) ServletActionContext.getRequest().getSession().getAttribute(model.getTelephone());//session中的验证码
+		//如果验证不通过
+		if(StringUtils.isBlank(checkcode)||!checkcode.equals(sessionCode)){
+			return INPUT;
+		}
+		String subject="bos商城信息";
+		String activeUrl="http://localhost:8080/bos_fore/customer_active.action";//激活的action
+		String activeCode = RandomStringUtils.randomNumeric(32);//随机码
+		String urlAddress=activeUrl+"?activeCode="+activeCode+"&telephone="+model.getTelephone();
+		String content="<h3>请点击地址激活:<a href="+urlAddress+">"+urlAddress;
+		String to=model.getEmail();
+		MailUtils.sendMail(subject, content, to);
+		WebClient.create("http://localhost:8002/crm_management/services/customerservice/customers")
+				.type(MediaType.APPLICATION_JSON)
+				.post(model);
+		redisTemplate.opsForValue().set(model.getTelephone(),activeCode,24,TimeUnit.HOURS);//将内容存入redis中
+		return SUCCESS;
+	}
+	 
+	    /**
+	     * 说明：获取url中的activecode和电话号码，判断active是否有效
+	     * 将activecode和redis中的active比较，如果成功就将电话号码对应的customer激活
+	     * @return
+	     * @author luowenxin
+	     * @time：2017年12月6日 下午3:30:14
+	     */
+	private String activeCode;
+	public void setActiveCode(String activeCode) {
+		this.activeCode = activeCode;
+	}
+	@Action("customer_active")
+	public String activeCustomer() throws IOException{
+		HttpServletResponse response = ServletActionContext.getResponse();
+		response.setContentType("text/html; charset=UTF-8");//响应乱码解决
+		String redisCode = redisTemplate.opsForValue().get(model.getTelephone());
+		//如果activecode不相同
+		if(StringUtils.isBlank(redisCode)||!redisCode.equals(activeCode)){
+			response.getWriter().println("对不起,验证码已经失效");
+			return NONE;
+		}
+		WebClient.create("http://localhost:8002/crm_management/services/customerservice/customers")
+		.path("/type")
+		.path("/"+model.getTelephone())
+		.path("/"+1)
+		.type(MediaType.APPLICATION_JSON)
+		.put(null);
+		response.getWriter().println("恭喜,激活成功");
+		redisTemplate.delete(model.getTelephone());//成功之后删除激活码
+		return NONE;
+	}
+}
---
+++ bos_fore/src/test/java/cn/bos/test/Test1.java
@@ -0,0 +1,21 @@
+package cn.bos.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext-cache.xml")
+public class Test1 {
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
+	@Test
+	public void test1(){
+		redisTemplate.opsForValue().set("username2", "小红");
+		//基本取值
+		System.out.println(redisTemplate.opsForValue().get("username2"));
+
+	}
+}
